63fcf4c7d00272a3c41f18bb9911a953b9e00e28
viraj gunasinghe <virajwgunasinghe@gmail.com>
2024-12-04 05:56:17 +0530

    update employee

--- ModernApp/MVC/View/EmployeeView.xaml.cs
+++ ModernApp/MVC/View/EmployeeView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LiveCharts.Definitions.Series;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ModernApp.MVVM.View
 {
     /// <summary>
@@ -23,6 +26,42 @@ namespace ModernApp.MVVM.View
         public EmployeeView()
         {
             InitializeComponent();
+            DataContext = this;
+           
         }
+
+
+
+        private int currentNewEmployees = 500;
+       
+
+        
+
+        //Change colors in the mployeeCount accordign to increment and decrement 
+        private void UpdateEmployeeCount(int newCount)
+        {
+            // Determine if the value is increasing or decreasing
+            var isIncreasing = newCount > currentNewEmployees;
+
+            // Update the TextBlock's value with the correct sign
+            totalNewEmployeesBlock.Text = (isIncreasing ? "+" : "-") + newCount;
+
+            // Change color based on the change
+            totalNewEmployeesBlock.Foreground = new SolidColorBrush(isIncreasing ? Colors.LightGreen : Colors.Red);
+
+            // Update the current value
+            currentNewEmployees = newCount;
+        }
+
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Simulate a change in the employee count
+            Random random = new Random();
+            int newCount = random.Next(-100, 1000); // Random new value
+            UpdateEmployeeCount(newCount);
+        }
+
+        
     }
 }
